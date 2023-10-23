dd481d591d9f643093a9923b00cc771a647440ea
Chase McDougall <chasemcdougall@hotmail.com>
2023-10-23 12:26:41 -0400

    require login for deletion

--- project/app.py
+++ project/app.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from flask import Flask, g, render_template, request, session, \
                   flash, redirect, url_for, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from functools import wraps
 
 
 basedir = Path(__file__).resolve().parent
@@ -27,11 +28,30 @@ db = SQLAlchemy(app)
 from project import models
 
 
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('logged_in'):
+            flash('Please log in.')
+            return jsonify({'status': 0, 'message': 'Please log in.'}), 401
+        return f(*args, **kwargs)
+    return decorated_function
+
+
 @app.route('/')
 def index():
     """Searches the database for entries, then displays them."""
     entries = db.session.query(models.Post)
     return render_template('index.html', entries=entries)
+
+
+@app.route('/search/', methods=['GET'])
+def search():
+    query = request.args.get("query")
+    entries = db.session.query(models.Post)
+    if query:
+        return render_template('search.html', entries=entries, query=query)
+    return render_template('search.html')
 
 
 @app.route('/add', methods=['POST'])
@@ -71,6 +91,7 @@ def logout():
 
 
 @app.route('/delete/<int:post_id>', methods=['GET'])
+@login_required
 def delete_entry(post_id):
     """Deletes post from database."""
     result = {'status': 0, 'message': 'Error'}
